8971801373d7ffb6a47cba966e9fa9ca1cdcfe0c
Sugitani Miwa <miwa.sugitani@hogeticlab.com>
2024-03-15 09:28:36 +0900

    今日のやること画面の作成

---
+++ screens/home.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import * as React from 'react';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('今日のやること')}
+      >
+        <Text style={styles.buttonText}>今日のやること</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    padding: 50,
+    borderRadius: 15,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
---
+++ screens/today.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text } from 'react-native';
+import * as React from 'react';
+
+export default function TodayScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 24 }}>今日のタスク</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
