d0f826ad4e5aabdbabf8465fee35184b5676d4bf
non <hatabungg3@gmail.com>
2024-03-17 16:07:03 +0900

    日付選択と選択した日付の取得

--- screens/other_today.js
+++ screens/other_today.js
@@ -3,11 +3,28 @@ import * as React from 'react';
 import { Calendar } from 'react-native-calendars';
 
 export default function Other_TodayScreen() {
+    const [selectedDate, setSelectedDate] = useState(null);
+    const navigation = useNavigation();
+
+    const dayPress = (day) => {
+      //選択された日付を取得
+      const selectedDate = day.dateString;
+
+      //日付をStateに設定
+      setSelectedDate(selectedDate);
+
+      //他のページに遷移
+      navigation.navigate('...', {selectedDate});
+
+    };
+
     return(
       <View style={styles.container}>
         <Text style={{ fontSize: 24 }}>今日以外のタスク</Text>
         <Calendar
           style = {styles.calendar}
+          markedDates={{[selectedDate]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' }}}
+          onDayPress={dayPress}
         />
       </View>
     );
@@ -22,5 +39,6 @@ const styles = StyleSheet.create({
   },
   calendar: {
     top: 50,
+    width: 300,
   },
 });
