5178a7519ae674bc70d1d4b334fe28a6296e13a5
non <hatabungg3@gmail.com>
2024-03-19 17:52:06 +0900

    年月日の文字追加

--- screens/otherday.js
+++ screens/otherday.js
@@ -3,9 +3,14 @@ import * as React from 'react';
 
 export default function OtherdayScreen({route}) {
     const selectedDate = route.params.selectedDate; // パラメータを取得
+
+    const year = selectedDate.slice(0, 4);
+    const date = selectedDate.slice(5, 7);
+    const day = selectedDate.slice(8, 10);
+
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>{selectedDate}のタスク</Text>
+      <Text style={{ fontSize: 24 }}>{year}年{date}月{day}日のタスク</Text>
     </View>
   );
 }
