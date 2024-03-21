1b6d19dcad7759c155b4c10493b96e2bc0fbaf6c
non <hatabungg3@gmail.com>
2024-03-21 22:04:55 +0900

    今日以外のやることの遷移を追加した

--- screens/otherday.js
+++ screens/otherday.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import db from "./firebase";
 import { collection, query, where, getDocs } from "firebase/firestore"; 
 
@@ -13,9 +13,16 @@ export default function OtherdayScreen({route}) {
   let day = selectedDate.slice(8, 10);
 
   //データ読み込み
-  const [posts,setPosts]=useState([]);
+  const [posts, setPosts] = useState([]);
+  //const route = useRoute();
+  const [who, setWho] = useState('');
 
   useEffect(()=>{
+
+    if (route.params?.who) {
+      setWho(route.params.who);
+    }
+
     const fetchData = async () => {
       try{
         //year, month, dayを文字列型から数値型へ変換
@@ -24,28 +31,22 @@ export default function OtherdayScreen({route}) {
         day = Number(day);
 
         // Firestoreのクエリで使用する開始と終了の日付を取得
-        let startDate = new Date(year, month, day);
-        let endDate = new Date(year, month, day + 1); // 終了日は翌日の0時とすることで、今日のデータのみを取得
-        console.log('startDate:', startDate);
-        console.log('endDate:', endDate);
+        let startDate = new Date(year, month - 1, day, 0, 0, 0);
+        startDate.setHours(startDate.getHours());
+        let endDate = new Date(year, month - 1, day + 1, 0, 0, 0); // 終了日は翌日の0時とすることで、今日のデータのみを取得
+        endDate.setHours(endDate.getHours());
 
         //クエリ
         const q = query(collection(db, "posts"), where("date", ">=", startDate), where("date", "<", endDate));
         const querySnapshot = await getDocs(q);
 
-        // 重複を排除するために Set を使用
-        const postDataSet = new Set();
         const data = [];
 
         querySnapshot.forEach((doc) => {
         const postData = doc.data();
         // タイムスタンプ型のdateフィールドを日付型に変換
         const date = postData.date.toDate(); // タイムスタンプを日付に変換
-        // 重複していないデータのみを追加
-        if (!postDataSet.has(postData.id)) {
-          postDataSet.add(postData.id);
-          data.push({ id: doc.id, ...postData, date });
-        }
+        data.push({ id: doc.id, ...postData, date });
       });
 
       setPosts(data);
@@ -57,7 +58,7 @@ export default function OtherdayScreen({route}) {
     };
 
     fetchData(); // fetchData 関数を呼び出す
-  }, []);
+  }, [selectedDate, route.params?.who]);// selectedDateをOther_today.jsに渡す＆依存配列にroute.params?.whoを追加
 
   // React Navigationのナビゲーションオブジェクトを取得
   const navigation = useNavigation();
@@ -69,7 +70,7 @@ export default function OtherdayScreen({route}) {
   // タスクをタップしたときの処理
   const handleTaskPress = (post) => {
     if (post.who) {
-      navigation.navigate('完了画面', { 
+      navigation.navigate('完了今日以外画面', { 
         id: post.id, 
         yarukoto: post.yarukoto,
         who: post.who,
@@ -79,13 +80,14 @@ export default function OtherdayScreen({route}) {
         dayOfWeek: dayOfWeek,
         });
     } else {
-      navigation.navigate('今日の詳細', { 
+      navigation.navigate('今日以外詳細画面', { 
         id: post.id, 
         yarukoto: post.yarukoto,
         year: year,
         month: month,
         day: day,
         dayOfWeek: dayOfWeek,
+        who: post.who || '',
       });
     }
   };
@@ -148,6 +150,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    padding: 40,
+    borderRadius: 10,
+    width: 350,
+  },
+  button_ma: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    padding: 40,
+    borderRadius: 10,
+    width: 350,
+  },
+  button_fa: {
     marginTop: 20,
     backgroundColor: 'white',
     padding: 40,
---
+++ screens/today_detail_otherday.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Today_Detail_OtherdayScreen({ route }) {
+  const navigation = useNavigation();
+  // const { taskId } = route.params;// タスクIDを取得
+  const {id, yarukoto, year, month, day, dayOfWeek} = route.params; // タスク名を取得
+  
+
+  // はいのボタンをタップした際の処理を記述
+  const handleYesPress = () => {
+    navigation.navigate('今日以外はい', {
+      id: id, 
+      yarukoto: yarukoto,
+      year: year,
+      month: month,
+      day: day,
+      dayOfWeek: dayOfWeek,
+    });
+  };
+  // 今日はやらないボタンをタップした際の処理を記述
+  const handleYaranaiPress = () => {
+    navigation.navigate('今日以外やらない');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.headerText, { fontSize: 40 , textAlign: 'center' }]}>
+      {`${month}月${day}日${dayOfWeek}の\nやること`}
+      </Text>
+      <View style={styles.taskDetailContainer}>
+      <Text style={styles.taskDetail}>{yarukoto}</Text>
+      </View>
+      <Text style={styles.questionText}>できましたか</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleYesPress}>
+          <Text style={styles.buttonText}>はい</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+          <Text style={styles.buttonText}>いいえ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleYaranaiPress}>
+          <Text style={styles.buttonText}>今日はやらない</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bba8e3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  taskDetail: {
+    fontSize: 50,
+    marginBottom: 20,
+  },
+  questionText: {
+    fontSize: 40,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 20,
+    padding: 50,
+    borderRadius: 10,
+    width: 250,
+    backgroundColor: 'pink',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 24, // フォントサイズを大きくする
+    textAlign: 'center', // 中央揃えにする
+  },
+  taskDetailContainer: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    width: 400,
+    marginBottom: 25,
+  },
+  taskDetail: {
+    fontSize: 50,
+    textAlign: 'center',
+  },
+});
